00a6eb86dce4faeca569ae21868da382318501b1
Dmitriy-Tyukh <roost1982@bigmir.net>
2022-09-29 15:50:59 +0300

    add gallery

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const galleryItem = document.querySelector('.gallery');
+const ItemsMarkup = galleryItemsMarkup(galleryItems);
+
+galleryItem.innerHTML = ItemsMarkup;
+
+let imageLightBox = new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function galleryItemsMarkup(gallery) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        ` <a class="gallery__item" href="${original}">
+            <img class="gallery__image" loading="lazy" src="${preview}" alt="${description}" />
+        </a> `
+    )
+    .join('');
+}
